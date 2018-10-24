dd88eabe8f209d66ab18bb27da4297efb609b704
furikake6000 <ewave63@keio.jp>
2018-10-24 13:56:34 +0900

    Statusページの追加（バグあり）

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def spotify
+    # WARNING: this won't work well.
+    RSpotify::User.new(spotifydata)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   get '/search', to: 'static_pages#search'
 
+  # User controller
+  get '/status', to: 'users#status'
+
   # Spotify API callback
   get '/auth/spotify/callback', to: 'users#spotify'
 
